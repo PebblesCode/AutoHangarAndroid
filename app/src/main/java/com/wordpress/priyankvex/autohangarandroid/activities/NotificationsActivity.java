914bb51c2f42cd4f5cb78b816cb2b7c8cdb8583d
Priyank Verma <priyankvex95@gmail.com>
2016-02-15 00:29:50 +0530

    Back menu button now calls onBackPressed()

--- app/src/main/java/com/wordpress/priyankvex/autohangarandroid/activities/NotificationsActivity.java
+++ app/src/main/java/com/wordpress/priyankvex/autohangarandroid/activities/NotificationsActivity.java
@@ -2,6 +2,7 @@ package com.wordpress.priyankvex.autohangarandroid.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.wordpress.priyankvex.autohangarandroid.R;
 
@@ -15,6 +16,26 @@ public class NotificationsActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("Notifications");
+        }
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }

fb0978a6e255a51022966dee26b7523bad81d916
Priyank Verma <priyankvex95@gmail.com>
2016-02-16 20:51:22 +0530

    Add search activity.

---
+++ app/src/main/java/com/wordpress/priyankvex/autohangarandroid/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.wordpress.priyankvex.autohangarandroid.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by @priyankvex on 16/2/16.
+ * Activity to search for service providers.
+ */
+public class SearchActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
